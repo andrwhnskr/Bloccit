d49d54f770bb8dd9687e8b534020711adc6ba148
Andrew Hunsaker <andrw.hnskr@gmail.com>
2014-06-14 13:57:09 -0600

    Created Post and Comment models with relationships

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,20 @@
 
 ActiveRecord::Schema.define(version: 20140614135915) do
 
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
